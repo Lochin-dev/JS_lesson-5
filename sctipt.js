e7e3b617234b455cd1e77b527c5147f37948c5b5
web-developer001 <lochinquvondiqov31@gmail.com>
2022-10-19 02:06:02 +0500

    lesson-5 JS

---
+++ sctipt.js
@@ -0,0 +1,174 @@
+// // 1-masala
+
+// let n = prompt("Butun son kiriting");
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   arr.push(i);
+//   console.log(arr[i])
+// }
+//  console.log(arr);
+
+// // 2-masala
+
+// let n = prompt("Butun son kiriting")
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   arr.push( 2 ** i)
+//   console.log(arr[i])
+// }
+// console.log(arr)
+
+// // 3-masala
+
+// let n =+ prompt("Butun son kiriting");
+// let x = 0;
+// let arr=[]
+// for (let i = 0; i <= n; i++) {
+// arr.push(x+=i)
+//   console.log(arr[x]);
+// }
+// console.log(arr)
+
+// // 4-masala
+
+// let n = +prompt("Butun son kiriting");
+// let sum=0;
+// let ayirmasi=0;
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   arr.push(i * 10);
+//   if(i%2==1){
+//     sum+=arr[i]
+//     console.log(sum+" toqlari yig'indisi");
+//   }else{
+//     ayirmasi-=arr[i]
+//     console.log(ayirmasi +" Juftlari ayirmasi")}
+// }
+// console.log(arr);
+
+// // 5-masala
+
+// let n =+ prompt("Butun son kiriting");
+// let x = 0;
+// let arr=[]
+// for (let i = 0; i <= n; i++) {
+// x+=i;
+// console.log(x +" Yig'indisi")
+// arr.push(i)
+//   console.log(arr[i]);
+// }
+// console.log(arr)
+
+// // 6-masala
+
+// let n = +prompt("Butun son kiriting");
+// let arr = []
+// for (let i = n; i >= 0; i--) {
+//   arr.push(i)
+// }
+// console.log(arr)
+
+// 7-masala ???????????????????????????????????????????????????????????????
+
+// // 8-masala  ?????????????????????????????????????????????????????????????
+// let n = 10;
+
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   arr.push(i);
+//   console.log(arr[i]);
+// }
+// console.log(arr);
+
+// let A = 3;
+// let B = 7;
+// const arre = [];
+// const y = 0;
+// const x = arr.slice(A, B);
+// for (let i = A; i <= B; i++) {
+//   arre.push(i)
+//   console.log(arre[i]);
+// }
+// console.log(arre);
+
+// // 9-masala
+
+// let n = +prompt("Butun son kiriting");
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 0) {
+//     arr.push(i);
+//   }
+// }
+// console.log(arr);
+// if (n % 2 == 0) {
+//   console.log([n]);
+// } else {
+//   console.log([n - 1]);
+// }
+
+// 10-masala
+
+// let n = +prompt("Butun son kiriting");
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 1) {
+//     arr.push(i);
+//   }
+// }
+// console.log(arr);
+
+
+// 11-masala
+
+// let n = +prompt("Butun son kiriting");
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 1) {
+//     arr.push(i);
+//   }
+// }
+// console.log(arr);
+// if (n % 2 == 1) {
+//   console.log([n]);
+// } else {
+//   console.log([n - 1]);
+// }
+
+// // 12-masala
+
+// let n = +prompt("Butun son kiriting");
+// let arr = [];
+// let x=0;
+// for (let i = 0; i <= n; i++) {
+//     arr.push(i);
+//     x+=i;
+//   }
+// console.log(arr);
+// console.log(x/n + " o'rta arifmetigi")
+
+
+// // 13-masala
+
+// let n = +prompt("n=");
+// let arr = [];
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 1) {
+//     arr.push(i);
+//     console.log(arr[i])
+//   }
+// }
+// console.log(arr);
+
+
+// 14-masala ??????????????????????????????????????????????????????
+
+
+
+// 15-masala
+
+
+
+// 16-masala
+
+// 2-masala
